5b5fa338e546dece90c2e2671021545ea43c5aad
sudhanarumugam <sudhanarumugam10153@gmial.com>
2023-10-12 16:44:23 +0530

    #

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 
 function App() {
   return (
     <>
     <Header/>
     <main className='py-3'>
-      <Container> <h1>Welcome To  Marvel Wokers</h1></Container>
+      <Container>
+        <Outlet/>
+      </Container>
    
     </main>
     <Footer/>
